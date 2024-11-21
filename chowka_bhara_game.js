1c7e73d42e14c75460d3795a15bd5e761205efcc
Amit Laitbar <118708536+Amitlaitbar@users.noreply.github.com>
2024-11-21 12:19:15 +0530

    Update chowka_bhara_game.js

--- chowka_bhara_game.js
+++ chowka_bhara_game.js
@@ -1,30 +1,44 @@
 // In this game only four players can play 
 
-function createPlayGround() {
-  let ground = "←←←←←←←←←←←←←←←←CHOWKA BHARA GAME→→→→→→→→→→\n";
-  ground += '┏━━━━━━━┳━━━━━━━┳━━━━━━━┳━━━━━━━┳━━━━━━━┓\n';
-  ground += '┃       ┃       ┃1️⃣    2️⃣ ┃       ┃       ┃\n';
-  ground += '┃       ┃       ┃       ┃       ┃       ┃\n';
-  ground += '┃       ┃       ┃3️⃣    4️⃣ ┃       ┃       ┃\n';
-  ground += '┣━━━━━━━╋━━━━━━━╋━━━━━━━╋━━━⇩━━━╋━━━━━━━┫\n';
-  ground += '┃       ┃       ┃       ┃       ┃       ┃\n';
-  ground += '┃       ⇨       ┃       ┃       ┃       ┃\n';
-  ground += '┃       ┃       ┃       ┃       ┃       ┃\n';
-  ground += '┣━━━━━━━╋━━━━━━━╋━━━⇩━━━╋━━━━━━━╋━━━━━━━┫\n';
-  ground += '┃ ➀   ➁ ┃       ┃       ┃       ┃ ➊   ➋ ┃\n';
-  ground += '┃       ┃       ⇨       ⇦       ┃       ┃\n';
-  ground += '┃ ➂   ➃ ┃       ┃       ┃       ┃ ➌   ➍ ┃\n';
-  ground += '┣━━━━━━━╋━━━━━━━╋━━━⇧━━━╋━━━━━━━╋━━━━━━━┫\n';
-  ground += '┃       ┃       ┃       ┃       ┃       ┃\n';
-  ground += '┃       ┃       ┃       ┃       ⇦       ┃\n';
-  ground += '┃       ┃       ┃       ┃       ┃       ┃\n';
-  ground += '┣━━━━━━━╋━━━⇧━━━╋━━━━━━━╋━━━━━━━╋━━━━━━━┫\n';
-  ground += '┃       ┃       ┃ ❶   ❷ ┃       ┃       ┃\n';
-  ground += '┃       ┃       ┃       ┃       ┃       ┃\n';
-  ground += '┃       ┃       ┃ ❸   ❹ ┃       ┃       ┃\n';
-  ground += '┗━━━━━━━┻━━━━━━━┻━━━━━━━┻━━━━━━━┻━━━━━━━┛';
-
-  console.log(ground);
+function moveForPlayer1(ground) {
+  for (let index = 0; index < ground.length; index++) {
+    if (ground[index] === "1") {
+      console.log('inside the check');
+      return index;
+    }
+  }
 }
 
-createPlayGround();
+function createPlayGround() {
+
+  let ground = '\n\t                P l a y e r 1              \n';
+  ground += '\t  ┏━━━━━━━┳━━━━━━━┳━━━━━━━┳━━━━━━━┳━━━━━━━┓ \n';
+  ground += '\t  ┃       ┃       ┃ 1   2 ┃       ┃       ┃ \n';
+  ground += '\t  ┃       ┃   ⇦   ┃       ┃       ┃       ┃ \n';
+  ground += '\t  ┃       ┃       ┃ 3   4 ┃       ┃       ┃ \n';
+  ground += '\t  ┣━━━━━━━╋━━━━━━━╋━━━━━━━╋━━━⇩━━━╋━━━━━━━┫ \n';
+  ground += '\t  ┃       ┃       ┃       ┃       ┃       ┃ \n';
+  ground += '\t  ┃       →       ┃       ┃       ┃   ⬆   ┃ \n';
+  ground += '\tp ┃       ┃       ┃       ┃       ┃       ┃ p\n';
+  ground += '\tl ┣━━━━━━━╋━━━━━━━╋━━━⇩━━━╋━━━━━━━╋━━━━━━━┫ l\n';
+  ground += '\ta ┃ ➀   ➁ ┃       ┃       ┃       ┃ ➊   ➋ ┃ a\n';
+  ground += '\ty ┃       ┃       →       ⬅       ┃       ┃ y\n';
+  ground += '\te ┃ ➂   ➃ ┃       ┃       ┃       ┃ ➌   ➍ ┃ e\n';
+  ground += '\tr ┣━━━━━━━╋━━━━━━━╋━━━⇡━━━╋━━━━━━━╋━━━━━━━┫ r\n';
+  ground += '\t2 ┃       ┃       ┃       ┃       ┃       ┃ 4\n';
+  ground += '\t  ┃   ↓   ┃       ┃       ┃       ⬅       ┃ \n';
+  ground += '\t  ┃       ┃       ┃       ┃       ┃       ┃ \n';
+  ground += '\t  ┣━━━━━━━╋━━━⇡━━━╋━━━━━━━╋━━━━━━━╋━━━━━━━┫ \n';
+  ground += '\t  ┃       ┃       ┃ ❶   ❷ ┃       ┃       ┃ \n';
+  ground += '\t  ┃       ┃       ┃       ┃   ⇢   ┃       ┃ \n';
+  ground += '\t  ┃       ┃       ┃ ❸   ❹ ┃       ┃       ┃ \n';
+  ground += '\t  ┗━━━━━━━┻━━━━━━━┻━━━━━━━┻━━━━━━━┻━━━━━━━┛ \n';
+  ground += '\t                P l a y e r 3               \n';
+
+ return ground;
+}
+
+const ground = createPlayGround();
+console.log(ground);
+
+console.log(moveForPlayer1(ground));
