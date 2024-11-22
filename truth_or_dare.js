d873fe80dc7c1bb27faaa351ced9916f0d5c26c6
Amit Laitbar <118708536+Amitlaitbar@users.noreply.github.com>
2024-11-22 10:30:29 +0530

    Update truth_or_dare.js

--- truth_or_dare.js
+++ truth_or_dare.js
@@ -1,13 +1,15 @@
 let prevTaskNum = 0;
+
 function generateTaskNumber() {
   const taskNum = Math.round(Math.random() * 9 + 1);
+
   if (prevTaskNum !== taskNum) {
     return taskNum;
   }
+
   prevTaskNum = taskNum;
   generateTaskNumber();
 }
-
 
 function truth(taskNum) {
   switch (taskNum) {
@@ -16,9 +18,9 @@ function truth(taskNum) {
     case 2:
       return "What is the biggest secret you're hiding from your parents?";
     case 3:
-      return "When was the last time you cried?";
+      return "When was the last time you cried? 😭";
     case 4:
-      return "What's the most embarrassing music you listen to?";
+      return "What's the most embarrassing music you listen to? 🎧🎼";
     case 5:
       return "What is your biggest regret in life?";
     case 6:
@@ -26,11 +28,11 @@ function truth(taskNum) {
     case 7:
       return "If you could be invisible for a day, what's the first thing you would do?";
     case 8:
-      return "If you suddenly had a million pounds, how would you spend it?";
+      return "If you suddenly had a million pounds, how would you spend it? 🤔";
     case 9:
       return "When was the last time you helped a stranger?";
     case 10:
-      return "Do you believe in ghosts?";
+      return "Do you believe in ghosts? 👻";
   }
 }
 
@@ -47,37 +49,50 @@ function dare(taskNum) {
     case 5:
       return "Call the first person in your phonebook and howl like a wolf.📞";
     case 6:
-      return "Give up your phone for the rest of the day."
+      return "Give up your phone for the rest of the day.😇"
     case 7:
-      return "Let someone else tickle you and try not to laugh";
+      return "Let someone else tickle you and try not to laugh.😅";
     case 8:
-      return "Keep your eyes closed until it’s your turn again.";
+      return "Keep your eyes closed until it’s your turn again.🙈🙈";
     case 9:
-      return "sing song for whome next to you {should be romatic song}";
+      return "Sing song for whome next to you {It should be romatic song}.🎶🎵";
     case 10:
-      return "do 10 push without failure if fails, start again";
+      return "Do 10 push without failure if fails, start again.😈";
   }
 }
-
 
 function playerSelection(numberOfPlayers) {
   return Math.round(Math.random() * (numberOfPlayers - 1) + 1);
 }
 
 function truthOrDare(taskNum) {
-  const isTrue = confirm("\n\tEnter 'y' for truth and 'Press Enter' for Dare:");
+  const input = prompt('\nEnter "Truth" for truth and "Dare" for dare : ');
 
-  if (isTrue) {
-    return '\nTruth => ' + truth(taskNum) + '\n';
-  } else {
-    console.log('\n Dare => ' + dare(taskNum));
-    return prompt('\n\t**** Press "Enter" After Task completion !!');
+  if (input === 'Truth') {
+    console.log('\n----------------------------------------------------------');
+    console.log('Your Truth Task is ☞ "' + truth(taskNum) + '"');
+    console.log('----------------------------------------------------------');
+
+    return prompt('\n\t**** Press "Enter" After Truth Task completion !!');
   }
 
+  if (input === 'Dare') {
+    console.log('\n----------------------------------------------------------');
+    console.log('Your Dare Task is ☞ "' + dare(taskNum) + '"');
+    console.log('----------------------------------------------------------');
+
+    return prompt('\n\t**** Press "Enter" After Dare Task completion !!');
+  } else {
+    console.log('\n------------------------------');
+    console.log('| ❌ Enter valid Input !!        |');
+    console.log('------------------------------');
+  }
+
+  return truthOrDare(taskNum);
 }
 
 function roundInfo(playerNum, taskNum, noOfturn, roundLimit) {
-  console.log("\tIt's player no. " + playerNum + "'s turn");
+  console.log("\t\nIt's player no. " + playerNum + "'s turn");
   console.log(truthOrDare(taskNum));
 
   if (noOfturn === roundLimit - 1) {
@@ -90,8 +105,8 @@ function roundInfo(playerNum, taskNum, noOfturn, roundLimit) {
 function play(numberOfPlayers, noOfturn, roundLimit) {
   if (noOfturn > roundLimit) {
     const wantsToContinue = confirm('Do you want to continue the game ?');
-
     const endMessage = '\n\tThanks for Playing...🙂 Have a Good Day... 😊\n';
+
     return wantsToContinue ? play(numberOfPlayers, 0, 5) : endMessage;
   }
 
